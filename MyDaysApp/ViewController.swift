064922ef81ffe7497864ca700bcb84933d86583f
Egor Ukolov <ukolovegor@yandex.ru>
2023-08-04 19:49:00 +0700

    Реализация логики приложения и отображения результата на экране

--- MyDaysApp/ViewController.swift
+++ MyDaysApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var infoLabel: UILabel!
+    //@IBOutlet var resultButton: UIButton!
+    
+    private var numberOfDays = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       // resultButton.layer.cornerRadius = 12
     }
 
-
+    @IBAction func resultButtonTapped() {
+        infoLabel.text = "Ты наслаждаешься жизнью уже \(numberOfDays)"
+    }
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let range = sender.date..<Date.now
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+    }
+    
 }
 
